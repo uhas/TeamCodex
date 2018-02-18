6a6488dfcff549d8c1faf0dd49a98b905fdb4a64
sandeep dara <darasandeep7@gmail.com>
2018-02-18 13:27:25 -0600

    Added feedbacks and delete functionality working commit was done late because of working software

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var routes=require('./server/routes');
 
 
 app.locals.pretty = true;
-app.set('port', process.env.PORT || 3006);
+app.set('port', process.env.PORT || 3030);
 // app.set('views', __dirname + '/app/server/views');
 
 // my new code
